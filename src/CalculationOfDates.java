8b4352a31e307b17511f20561532dc74034680f1
Murad <muradbaki09072003@gmail.com>
2024-12-06 20:10:28 +0400

    Additional Task

--- src/CalculationOfDates.java
+++ src/CalculationOfDates.java
@@ -6,17 +6,20 @@ public class CalculationOfDates {
 
     public static void main(String[] args) {
 
-        String str = "09/07/2003";
-        String str2 = "07/12/2024";
+        String str = "09/07/2003"; //first date in String type
+        String str2 = "07/12/2024"; // second date in String type
 
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 
-        LocalDate temp = LocalDate.from(formatter.parse(str));
+        LocalDate temp = LocalDate.from(formatter.parse(str)); //creating a localdate object from a string format.
         LocalDate temp2 = LocalDate.from(formatter.parse(str2));
 
+
+        // printing all the days from the first date to the last date,converting it to a list and printing its size to get the exact number.
         System.out.println(temp.datesUntil(temp2).toList().size());
+
 
 
     }
